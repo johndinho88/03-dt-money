fc4c8ca4bb9bbb7ae563caaa6d99c75ae15b80a6
Jon Anderson <johndinho88@gmail.com>
2023-11-22 17:22:21 -0300

    Dialog

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,9 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
+import * as Dialog  from '@radix-ui/react-dialog';
+
 import logoImg from "../../assets/logo.svg";
+
 
 export function Header() {
   return (
@@ -8,7 +11,24 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay />
+
+          <Dialog.Content>
+            <Dialog.Title>Nova Transação</Dialog.Title>
+
+            <Dialog.Close />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+
+      
+
       </HeaderContent>
     </HeaderContainer>
   );
